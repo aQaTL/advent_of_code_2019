8f4d5bbed9d0847afcacc55d18a0f46ddb9f3f85
aQaTL <mmsoltys@outlook.com>
2019-12-29 22:52:15 +0100

    Day 21

---
+++ day_21/src/main.rs
@@ -0,0 +1,53 @@
+use intcode::Intcode;
+use std::sync::mpsc::channel;
+
+macro_rules! send {
+	($sender:expr,{$($instr:tt $x:tt $y:tt)*}, $run_cmd:tt) => {
+		concat!(
+			$(concat!(stringify!($instr $x $y), '\n'),)*
+			concat!(stringify!($run_cmd), '\n')
+		)
+		.bytes().for_each(|b| $sender.send(b as i64).unwrap());
+	};
+}
+
+fn main() {
+	let mut interpreter =
+		Intcode::new(Intcode::parse_input(concat!(env!("CARGO_PKG_NAME"), "/input.txt")).unwrap());
+	let mut interpreter_2 = interpreter.clone();
+
+	let ((sender_p1, r), (s, receiver_p1)) = (channel(), channel());
+	std::thread::spawn(move || interpreter.run(s, r));
+	let ((sender_p2, r), (s, receiver_p2)) = (channel(), channel());
+	std::thread::spawn(move || interpreter_2.run(s, r));
+
+	send!(
+		&sender_p1,
+		{
+			NOT C J
+			AND D J
+			NOT A T
+			OR T J
+		},
+		WALK
+	);
+
+	send!(
+		&sender_p2,
+		{
+			NOT B T
+			NOT C J
+			OR T J
+			NOT A T
+			OR T J
+			AND D J
+			OR E T
+			OR H T
+			AND T J
+		},
+		RUN
+	);
+
+	println!("Part 1: {}", receiver_p1.iter().max().unwrap());
+	println!("Part 2: {}", receiver_p2.iter().max().unwrap());
+}
